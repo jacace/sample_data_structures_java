cb875ef17cb950e89be9a312ab2ad78df8a1ec01
jacace <javierandrescaceres@gmail.com>
2022-04-12 12:31:09 +0100

    added samples to check for valid java code

---
+++ src/main/java/validations/Person.java
@@ -0,0 +1,10 @@
+package validations;
+
+import java.util.Date;
+
+//The following class is jus to check if the java code is valid
+class Student {
+    String[] name = new String[1];
+    String[] surname;
+    Date[] birthDate[];
+}
