4a4549fa7ab43217c501457da91fe93c83b1584b
jacace <javierandrescaceres@gmail.com>
2022-04-12 12:37:24 +0100

    added validation for simple inheritance

--- src/main/java/validations/Person.java
+++ src/main/java/validations/Person.java
@@ -2,9 +2,15 @@ package validations;
 
 import java.util.Date;
 
-//The following class is jus to check if the java code is valid
-class Student {
+//The following class is just to check if the java code is valid
+class Student extends Human {
     String[] name = new String[1];
     String[] surname;
     Date[] birthDate[];
+    Float quota;
+}
+
+class Human
+{
+   String gender; 
 }
